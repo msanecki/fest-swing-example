28817ecad044e927034a18e16740388d030e6c1f
msanecki <marcin.sanecki@gmail.com>
2013-01-25 16:32:44 +0100

    Program ran in EDT Test should be also run in EDT but I get exception

--- src/test/java/pl/keepcoding/swing/custom/learningtest/JOutlookFromL2FProdTest.java
+++ src/test/java/pl/keepcoding/swing/custom/learningtest/JOutlookFromL2FProdTest.java
@@ -1,10 +1,10 @@
 package pl.keepcoding.swing.custom.learningtest;
 
-import java.awt.Point;
-
 import javax.swing.JTabbedPane;
+import javax.swing.SwingUtilities;
 
-import org.fest.swing.core.MouseButton;
+import org.fest.swing.annotation.RunsInEDT;
+import org.fest.swing.edt.GuiTask;
 import org.fest.swing.finder.WindowFinder;
 import org.fest.swing.fixture.FrameFixture;
 import org.fest.swing.launcher.ApplicationLauncher;
@@ -37,7 +37,6 @@ public class JOutlookFromL2FProdTest extends FestSwingTestCaseTemplate{
 
 	@After
     public void tearDown(){
-		robot().settings().delayBetweenEvents(1000);
         cleanUp();
     }
 	
@@ -45,47 +44,22 @@ public class JOutlookFromL2FProdTest extends FestSwingTestCaseTemplate{
 	@Test
 	public void outlookTabsClick() throws Exception {
 		
-		String[] tabTitles = mainFrame.tabbedPane(ComponentNames.outlookbar).tabTitles();
+		final JTabbedPane jTabbedPane = mainFrame.tabbedPane(ComponentNames.outlookbar).component();
+		GuiTask task = new GuiTask() {
+			
+			@Override
+			protected void executeInEDT() throws Throwable {
+				System.out.println("Is EDT: "+SwingUtilities.isEventDispatchThread());
+				jTabbedPane.setSelectedIndex(0);
+				Thread.sleep(1000);
+				jTabbedPane.setSelectedIndex(1);
+				Thread.sleep(1000);
+				jTabbedPane.setSelectedIndex(2);
+				Thread.sleep(1000);
+				
+			}
+		};
 		
-		JTabbedPane component = mainFrame.tabbedPane(ComponentNames.outlookbar).component();
-		component.setSelectedIndex(0);
-		Thread.sleep(1000);
-		component.setSelectedIndex(1);
-		Thread.sleep(1000);
-		component.setSelectedIndex(2);
-		Thread.sleep(1000);
-		
-		
-		for (String stringtitle : tabTitles) {
-			System.out.println(stringtitle);
-		}
-		
-		// TODO How to get access to tabbed panes from JOutlookBar ???
-		
-		int outlookBarXposition = 100;
-		int step = 30;
-		int nextPane = 752;
-		int xAxis = -1158;
-		
-		
-////		robot().moveMouse(xAxis,219);
-//		robot().click(new Point(xAxis,219), MouseButton.LEFT_BUTTON, 1);
-////		robot().moveMouse(xAxis,752);
-//		robot().click(new Point(xAxis,752), MouseButton.LEFT_BUTTON, 1);
-//		nextPane+=step;
-////		robot().moveMouse(xAxis,nextPane);
-//		robot().click(new Point(xAxis,nextPane), MouseButton.LEFT_BUTTON, 1);
-//		nextPane+=step;
-////		robot().moveMouse(xAxis,nextPane);
-//		robot().click(new Point(xAxis,nextPane), MouseButton.LEFT_BUTTON, 1);
-//		nextPane+=step;
-////		robot().moveMouse(xAxis,nextPane);
-//		robot().click(new Point(xAxis,nextPane), MouseButton.LEFT_BUTTON, 1);
-//		nextPane+=step;
-////		robot().moveMouse(xAxis,nextPane);
-//		robot().click(new Point(xAxis,nextPane), MouseButton.LEFT_BUTTON, 1);
-		
-		
-		
+		task.run();
 	}
 }
